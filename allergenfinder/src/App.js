13291d531521a472a41a5830aec52b28b0a7f135
shahanakhundmir <58731479+shahanakhundmir@users.noreply.github.com>
2021-03-25 11:16:26 +0000

    DropdownMenu component for restaurants

---
+++ allergenfinder/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import DropdownMenu from "./components/DropdownMenu"
+import "./App.css";
+
+function App() {
+
+  return (
+    <div>
+      <DropdownMenu />
+    </div>)
+}
+
+export default App;
