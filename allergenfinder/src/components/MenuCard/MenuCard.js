8e2ce14883151ba645b58522e36d57da3900a82a
shahanakhundmir <58731479+shahanakhundmir@users.noreply.github.com>
2021-04-02 18:00:16 +0100

    card layout changes

--- allergenfinder/src/components/MenuCard/MenuCard.js
+++ allergenfinder/src/components/MenuCard/MenuCard.js
@@ -9,33 +9,40 @@ import './MenuCard.css'
 
 function MenuCard() {
     const cardInfo = [
-        { id: "001", title: "Halloumi Sticks & Dip", discription: "Five chunky sticks of grilled halloumi with chilli jam for dipping...", image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/02/26/13/halloumisticks.jpg?width=990&auto=webp&quality=75", allergen: "/images/image14.png", tip: "Lupin" },
-        { id: "002", title: "5 Chicken Wings", discription: "Flame-grilled, infused with PERi-PERi and perfect for pairing with sides. Served...", image: "https://cdn.leitesculinaria.com/wp-content/uploads/2012/06/chinese-five-spice-chicken-wings-fp.jpg.optimal.jpg", allergen: "/images/image2.png", tip: "Celery" },
-        { id: "003", title: "Sunset Burger", discription: "Two chicken thighs, melting cheddar cheese, smoky red pepper chutney with lettuce ...", image: "https://www.nandos.co.uk/sites/default/files/M_BPW_SunsetBurger_smaller.jpg", allergen: "/images/image3.png", tip: "Treenuts" },
-        { id: "004", title: "The Great Imitator Wrap", discription: "Our PERi-Plant strips are made from pea-protein but taste...", image: "https://www.nandos.co.uk/sites/default/files/wrap_live.png", allergen: "/images/image1.png", tip: "Gluten" },
+        { id: "001", title: "Halloumi Sticks & Dip", description: "Five chunky sticks of grilled halloumi with chilli jam for dipping...", image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/02/26/13/halloumisticks.jpg?width=990&auto=webp&quality=75", allergen: "/images/image14.png", tip: "Lupin" },
+        { id: "002", title: "5 Chicken Wings", description: "Flame-grilled, infused with PERi-PERi and perfect for pairing with sides. Served...", image: "https://cdn.leitesculinaria.com/wp-content/uploads/2012/06/chinese-five-spice-chicken-wings-fp.jpg.optimal.jpg", allergen: "/images/image2.png", tip: "Celery" },
+        { id: "003", title: "Sunset Burger", description: "Two chicken thighs, melting cheddar cheese, smoky red pepper chutney with lettuce ...", image: "https://www.nandos.co.uk/sites/default/files/M_BPW_SunsetBurger_smaller.jpg", allergen: "/images/image3.png", tip: "Treenuts" },
+        { id: "004", title: "The Great Imitator Wrap", description: "Our PERi-Plant strips are made from pea-protein but taste...", image: "https://www.nandos.co.uk/sites/default/files/wrap_live.png", allergen: "/images/image1.png", tip: "Gluten" },
     ]
     const renderCard = (card, index) => {
         return (
-            <Card key={index} className="menuCard">
-                <Card.Img src={card.image} style={{ height: "8rem", width: "10rem", objectFit: "cover" }} />
-                <Card.Body>
+            <Card key={index} className="menu-card">
+                <Card.Img className='card-image' src={card.image}/>
+                <Card.Body classname="card-body">
                     <Card.Title>{card.title}</Card.Title>
-                    <Card.Text>{card.discription}
-                        <div style={{ width: "auto", marginTop: "0px" }}>
-                            <Badge>
-                                <OverlayTrigger overlay={<Tooltip id='tooltip-top'>{card.tip}</Tooltip>}>
-                                    <img src={card.allergen} alt="" className="allergenImg"></img>
-                                </OverlayTrigger>
-                            </Badge>
-                        </div>
-                    </Card.Text>
+                    <Card.Footer className="card-footer">
+                        <Badge className="card-badge">
+                            <OverlayTrigger overlay={<Tooltip id='tooltip-top'>{card.tip}</Tooltip>}>
+                                <img src={card.allergen} alt="" className="allergen-img"></img>
+                            </OverlayTrigger>
+                        </Badge>
+                    </Card.Footer>
+                    <Card.Text className="card-text">{card.description}</Card.Text>
                 </Card.Body>
             </Card>)
     }
     return (
-        <div className="menuCardClass">
+        <div>
             {cardInfo.map(renderCard)}
         </div>
     );
 }
 export default MenuCard;
+
+/**<Card.Footer className="card-footer">
+                        <Badge className="card-badge">
+                            <OverlayTrigger overlay={<Tooltip id='tooltip-top'>{card.tip}</Tooltip>}>
+                                <img src={card.allergen} alt="" className="allergen-img"></img>
+                            </OverlayTrigger>
+                        </Badge>
+                    </Card.Footer> */
