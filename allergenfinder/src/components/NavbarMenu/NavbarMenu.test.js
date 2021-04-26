50c8e4acf11538b675f73e1e1fdccf71a8e5266d
willian <williancarareto@gmail.com>
2021-04-26 17:48:02 +0100

    Navbar - Test Component

--- allergenfinder/src/components/NavbarMenu/NavbarMenu.test.js
+++ allergenfinder/src/components/NavbarMenu/NavbarMenu.test.js
@@ -2,19 +2,30 @@
 /* eslint-disable no-restricted-globals */
 import NavbarMenu from './NavbarMenu'
 import userEvent from '@testing-library/user-event'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+
 
 describe('NavbarMenu component', () => {
     test(`
-        Given the required props,
+        Once click in one of the submenu options ,
         The submenu shoud be present`, () => {
-            const mockClickFunction = jest.fn()
-            const getProps = {
-                selectSubMenu: mockClickFunction
-            }
-            render(<NavbarMenu { ...getProps} />)
-            const clickButton = screen.getProps("button").find(button => button.textContent = "subMenu" )
-            userEvent.click(clickButton)
-            expect(mockClickFunction).toHaveBeenLastCalledWith("001")
-        })
+        const mockFunction = jest.fn()
+        const getProps = {
+            selectSubMenu: mockFunction,
+            //selectSubMenu: () => {}
+        }
+
+        render(<NavbarMenu {...getProps} />)
+
+        const clickMenu = screen.getByText('Starters')
+        userEvent.click(clickMenu)
+
+        expect(mockFunction).toHaveBeenCalledWith('Starters')
+        expect(screen.getByText('Mains')).toBeInTheDocument()
+        expect(screen.getByText('Sides')).toBeInTheDocument()
+        expect(screen.getByText('Extras')).toBeInTheDocument()
+        expect(screen.getByText('Desserts')).toBeInTheDocument()
+        expect(screen.getByText('Drinks')).toBeInTheDocument()
+
     });
+})
